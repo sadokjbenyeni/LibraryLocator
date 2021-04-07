10f169a7610e2818c6a071d5021832ddadfd021c
sadok jbenyeni <sadok.jbenyeni@protonmail.com>
2021-04-07 13:32:17 +0100

    Built the application for production

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'app_public', 'src')));
+app.use(express.static(path.join(__dirname, 'app_public', 'build')));
 app.use('/', indexRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
