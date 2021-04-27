c1b394f8b13b5e59c9e0be7caf3d9b4f9ef15d53
sadok jbenyeni <sadok.jbenyeni@protonmail.com>
2021-04-27 14:26:58 +0100

    Worked on switch theme feature

--- app_public/src/app/home-list/home-list.component.ts
+++ app_public/src/app/home-list/home-list.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 @Component({
   selector: 'app-home-list',
   templateUrl: './home-list.component.html',
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeListComponent implements OnInit {
 
-  constructor() { }
+  private static readonly DARK_THEME_CLASS = 'dark-theme';
+  private static readonly DARK_THEME_LIGHT = 'light';
+  private static readonly DARK_THEME_DARK = 'dark';
+
+  public theme = HomeListComponent.DARK_THEME_DARK;
+
+  constructor(@Inject(DOCUMENT) private document: Document) {
+  }
 
   ngOnInit(): void {
   }
 
+  public toggleTheme(): void {
+    if (this.theme === HomeListComponent.DARK_THEME_LIGHT) {
+      this.document.documentElement.classList.remove(HomeListComponent.DARK_THEME_CLASS);
+      this.theme = 'dark';
+    }
+    else if (this.theme === HomeListComponent.DARK_THEME_DARK) {
+      this.document.documentElement.classList.add(HomeListComponent.DARK_THEME_CLASS);
+      this.theme = 'light';
+    }
+  }
+  // public selectDarkTheme(): void {
+  //   this.document.documentElement.classList.add(HomeListComponent.DARK_THEME_CLASS);
+  //   this.theme = HomeListComponent.DARK_THEME_DARK;
+  // }
+
+  // public selectLightTheme(): void {
+  //   this.document.documentElement.classList.remove(HomeListComponent.DARK_THEME_CLASS);
+  //   this.theme = HomeListComponent.DARK_THEME_LIGHT;
+  // }
 }
