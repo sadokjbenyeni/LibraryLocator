21005e929e8058166d2ac0f669bd13f174fff7de
sadok jbenyeni <sadok.jbenyeni@protonmail.com>
2021-05-04 11:49:36 +0100

    Fixed an issue related to dark theme that doesnt persist when reloading

--- app_public/src/app/home-list/home-list.component.ts
+++ app_public/src/app/home-list/home-list.component.ts
@@ -10,26 +10,35 @@ export class HomeListComponent implements OnInit {
   private static readonly DARK_THEME_CLASS = 'dark-theme';
   private static readonly DARK_THEME_LIGHT = 'light';
   private static readonly DARK_THEME_DARK = 'dark';
-
-  public theme = HomeListComponent.DARK_THEME_DARK;
+  public theme = localStorage.getItem('theme-value');
   public mobile = false;
   constructor(@Inject(DOCUMENT) private document: Document) {
   }
 
   ngOnInit(): void {
+    this.initTheme();
     if (window.screen.width < 600) {
       this.mobile = true;
     }
   }
 
-  public toggleTheme(): void {
-    if (this.theme === HomeListComponent.DARK_THEME_LIGHT) {
-      this.document.documentElement.classList.remove(HomeListComponent.DARK_THEME_CLASS);
-      this.theme = 'dark';
-    }
-    else if (this.theme === HomeListComponent.DARK_THEME_DARK) {
+  public initTheme(): void {
+    if (localStorage.getItem('theme-value') === HomeListComponent.DARK_THEME_DARK) {
       this.document.documentElement.classList.add(HomeListComponent.DARK_THEME_CLASS);
-      this.theme = 'light';
+    }
+  }
+
+  public toggleTheme(): void {
+    if (this.theme === HomeListComponent.DARK_THEME_DARK) {
+      this.document.documentElement.classList.remove(HomeListComponent.DARK_THEME_CLASS);
+      this.theme = HomeListComponent.DARK_THEME_LIGHT;
+      localStorage.setItem('theme-value', HomeListComponent.DARK_THEME_LIGHT);
+
+    }
+    else if (this.theme === HomeListComponent.DARK_THEME_LIGHT) {
+      this.document.documentElement.classList.add(HomeListComponent.DARK_THEME_CLASS);
+      this.theme = HomeListComponent.DARK_THEME_DARK;
+      localStorage.setItem('theme-value', HomeListComponent.DARK_THEME_DARK);
     }
   }
 }
