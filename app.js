074bf75eab64885901226cb7267d1ed2d37afc00
sadok jbenyeni <sadok.jbenyeni@protonmail.com>
2021-09-26 12:16:27 +0100

    Testing some feauture

--- app.js
+++ app.js
@@ -14,11 +14,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'app_public', 'build')));
 app.use(cors());
-// app.use('/api', (req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   next();
-// })
+app.use('/api', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+})
 app.use('/api', indexRouter);
 app.get('*', function (req, res, next) {
   app.use(express.static(path.json(__dirname, 'app_public', 'build', 'index.html')));
